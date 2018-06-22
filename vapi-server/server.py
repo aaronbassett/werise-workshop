a97044c04cd6a5de2217069df36af8ff60b1da1e
Aaron Bassett <me@aaronbassett.com>
2018-06-21 21:53:44 -0400

    Create recordings webhook endpoint

--- vapi-server/server.py
+++ vapi-server/server.py
@@ -1,4 +1,8 @@
 import os
+import time
+import uuid
+import jwt
+import requests
 import hug
 
 
@@ -8,8 +12,41 @@ def echo():
         {"action": "talk", "text": "Record your message after the beep"},
         {
             "action": "record",
-            "eventUrl": [f"{os.environ['VAPI_SERVER_URL']}/recordings"],
+            "eventUrl": [f"{os.environ['VAPI_SERVER_URL']}/v1/recordings"],
             "endOnKey": "*",
             "beepStart": True,
         },
     ]
+
+
+@hug.post("/recordings", versions=1)
+def recordings(recording_url, recording_uuid):
+    iat = int(time.time())
+
+    with open("private.key", "rb") as key_file:
+        private_key = key_file.read()
+
+    payload = {
+        "application_id": os.environ["APPLICATION_ID"],
+        "iat": iat,
+        "exp": iat + 60,
+        "jti": str(uuid.uuid4()),
+    }
+
+    token = jwt.encode(payload, private_key, algorithm="RS256")
+
+    recording_response = requests.get(
+        recording_url,
+        headers={"Authorization": b"Bearer " + token, "User-Agent": "voice-journal"},
+    )
+    if recording_response.status_code == 200:
+        recordingfile = f"./recordings/{recording_uuid}.mp3"
+        os.makedirs(os.path.dirname(recordingfile), exist_ok=True)
+
+        with open(recordingfile, "wb") as f:
+            f.write(recording_response.content)
+
+    return {}
+
+
+hug.API(__name__).http.serve(port=8800)
